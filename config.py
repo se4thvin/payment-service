ea06b48f6e3dab0aaa9a00170af42367edc3100d
Sethvin Nanayakkara <nanasethvin@gmail.com>
2025-01-10 14:13:20 -0600

    updated config

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
-# config.py
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    QUICKBOOKS_CLIENT_ID: str
-    QUICKBOOKS_CLIENT_SECRET: str
-    QUICKBOOKS_REDIRECT_URI: str
-    QUICKBOOKS_ENVIRONMENT: str  # 'sandbox' or 'production'
-    QUICKBOOKS_COMPANY_ID: str
-    DATABASE_URL: str
-    SECRET_KEY: str  # For JWT; should match Auth-service
+    CLIENT_ID: str
+    CLIENT_SECRET: str
+    REDIRECT_URI: str
+    ENVIRONMENT: str  # 'sandbox' or 'production'
+    COMPANY_ID: str
+    PAYMENT_DB_URI: str
+    SECRET_KEY: str  # For JWT; should match auth-service
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from config import settings
 
-DATABASE_URL = os.getenv("PAYMENT_DB_URI")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(settings.PAYMENT_DB_URI, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
